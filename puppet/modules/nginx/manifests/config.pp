7106c3b57c12c67f24b60a954c42d35edea5a247
Mick Pollard <aussielunix@gmail.com>
2011-10-23 19:03:42 +1100

    removed un-necessary code/files

--- puppet/modules/nginx/manifests/config.pp
+++ puppet/modules/nginx/manifests/config.pp
@@ -7,13 +7,4 @@ class nginx::config {
     require => Class['nginx::install'],
     notify  => Class['nginx::service'],
   }
-
-  file {'/etc/nginx/conf.d/server_names_hash_bucket_size.conf':
-    source  => 'puppet:///modules/nginx/server_names_hash_bucket_size.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Class['nginx::install'],
-    notify  => Class['nginx::service'],
-  }
 }
